26213e2235fb97e6e1708bee3e553c231f6fac02
Ahmed Elkashef <ahmedelkashef2012@gmail.com>
2018-02-20 04:52:46 +0200

    implement exit command

--- myshell.c
+++ myshell.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <stdlib.h>
+#include <string.h>
 
 extern char **getline();
 
@@ -11,7 +13,16 @@ int main() {
 
   while (1) {
     input = getline();
+
+    /* handling all cases of different commands */
     for (int i = 0; input[i] != NULL; ++i) {
+
+      /* exit the shell */
+      if ( strcmp(input[0],"exit") == 0 ) {
+	exit(-1);
+      }
+
+
       printf("Item %i of input: %s\n", i, input[i]); // The input list that must be parsed. 
     }
   }
