d063647cbb7258b962ce10c747d012df6d23ca89
Ahmed Elkashef <ahmedelkashef2012@gmail.com>
2018-02-22 20:15:40 +0200

    initial implementation of ls command

--- myshell.c
+++ myshell.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <dirent.h>
 
 extern char **getline();
 char *getcwd(char *buf, size_t size);
+char cwd[1024];
+
 
 int main() {
   int i;
   char **input;
 
   while (1) {
-    printf("\nMy Shell>> ");
+
+    if ( getcwd(cwd, sizeof(cwd) ) != NULL) {
+        printf("\nYou@My_Shell>> %s: ", cwd);
+    } else {
+	printf("\nMy Shell>> ");
+    }
     input = getline();
 
     /* handling all cases of different commands */
@@ -26,17 +36,29 @@ int main() {
 
       /* print current directory */
       if (strcmp(input[0],"cwd") == 0 ) {
-	char cwd[1024];
 	if (getcwd(cwd, sizeof(cwd)) != NULL) {
 	    printf("%s\n",cwd);
-	} /* else {
+	} else {
 	    perror("getcwd() Error");
-	} */
+	} 
       }
 
       /* changing current directory */
       if ( strcmp(input[0],"cd") == 0 ) {
 	chdir(input[1]);
+      }
+
+      /* list files in a directory */
+      if ( strcmp(input[0],"ls") == 0 ) {
+	char *pointer = NULL;
+	DIR *dp = NULL;
+	struct dirent *sd = NULL;
+	pointer = getenv("PWD");
+	dp = opendir((const char*)pointer);
+	while( (sd=readdir(dp)) != NULL ) {
+	    printf("%s \t", sd->d_name);
+	}
+	printf("\n");
       }
 
 
